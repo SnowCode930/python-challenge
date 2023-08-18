995239bb528991c0a4afe2c48d57cf065c506b3e
SnowCode930 <47792614+SnowCode930@users.noreply.github.com>
2023-08-18 08:53:54 -0800

    Add files via upload

---
+++ python-challenge/PyBank/main.py
@@ -0,0 +1,80 @@
+"""
+A (partial) Pandas-style solution for PyBank
+"""
+
+from statistics import mean as avg
+import os
+import csv
+
+# Assemble the input csv file path, starting from the cwd
+input_path = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
+
+# Open the budget_data csv file for reading
+with open(input_path, encoding="utf-8", newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Don't include the csv file header in the data
+    next(csv_reader)
+
+    # Pass the raw text data out as a list
+    csv_data = list(csv_reader)
+
+# Exract the month data and find the month count
+months = [record[0] for record in csv_data]
+print(months)
+num_months = len(months)
+print(num_months)
+
+# Extract the profit data and find the net profit
+profits = [int(record[1]) for record in csv_data]
+print(profits)
+net_profits = sum(profits)
+print(net_profits)
+
+# To generate difference data, first get a list of mmonths
+# and monthly differences for the second through last months
+monthly_differences = [
+    [months[i], profits[i] - profits[i - 1]] for i in range(1, num_months)
+]
+print(monthly_differences)
+
+# Find the average monthly change in profits
+avg_diff = avg(diff[1] for diff in monthly_differences)
+print(avg_diff)
+
+# Find the maximum monthy increase and the month in which it occurs
+max_incr = max(monthly_differences, key=lambda col: col[1])
+print(max_incr)
+
+# Find the maximum monthy decrease and the month in which it occurs
+max_decr = min(monthly_differences, key=lambda col: col[1])
+print(max_decr)
+
+# Create the financial analysis report text
+report = (
+    f"{' Financial Analysis ':-^48}\n"
+    f"{'Total Months:':24}{num_months:24,.0f}\n"
+    f"{'Net Profits:':24}{net_profits:24,.0f}\n"
+    f"{'Avg Change:':24}{avg_diff:24,.0f}\n"
+    f"{'Max Increase:':14}{max_incr[0]:^20}{max_incr[1]:14,.0f}\n"
+    f"{'Max Decrease:':14}{max_decr[0]:^20}{max_decr[1]:14,.0f}\n"
+    f"{'--':-^48}"
+)
+
+# Assemble the output text file path, starting from the cwd
+output_path = os.path.join(os.path.dirname(__file__), "Analysis", "budget_analysis.txt")
+
+# Open the budget_analysis text file and write the report to it
+with open(output_path, "w", encoding="utf-8") as textfile:
+    textfile.write(report)
+
+# Check to see if the report exists and is properly formatted:
+# The input and output resource paths are the same ... but we
+# we strive for clarity!
+input_path = output_path
+
+# Open the report text file for reading and print it to the terminal
+with open(input_path, "r", encoding="utf-8") as textfile:
+    report = textfile.read()
+
+print(f"\n\n{report}\n\n")
---
+++ python-challenge/PyPoll/main.py
@@ -0,0 +1,78 @@
+"""
+This script analyzes an election vote count data set and returns the following:
+
+1) The total number of votes cast
+2) A complete list of candidates who received votes
+3) The percentage of votes each candidate won
+4) The total number of votes each candidate won
+5) The winner of the election based on popular vote.
+
+All statistics are calculated concurrent with a single pass through the data.
+Since the variables are not known in advance, i.e., the names of the candidates
+who are up for election, mutable data types (lists and dictionaries) are used.
+"""
+
+# Import csv and os libraries to read/write
+import csv
+import os
+
+# Assemble the input csv file path, starting from the cwd
+input_path = os.path.join(os.path.dirname(__file__), "Resources", "election_data.csv")
+
+# Open the election_data csv file for reading
+with open(input_path, encoding="utf-8", newline="") as csvfile:
+    csvread = csv.reader(csvfile, delimiter=",")
+
+    # Skip header row since it is not valid data
+    next(csvread)
+
+    # Initialize the vote count dictionary
+    votes = {}
+
+    # Loop through the remaining records
+    for record in csvread:
+    
+        candidate = str(record[2])
+        votes[candidate] = votes.get(candidate, 0) + 1
+
+# Find the total number of votes
+total = sum(votes.values())
+
+# Find the name key of the maximum value in the dictionary
+winner = max(votes, key=votes.get)
+
+# Format the votes dictionary entries for the report
+lines = [
+    f"{k + ':':24}" f"{votes[k]:10,}" f"{votes[k]/total * 100:11,.2f}%\n"
+    for k in sorted(votes)
+]
+
+# Generate the report string text
+report = (
+    f"{' Election Results ':^46}\n"
+    f"{'--':-^46}\n"
+    f"{'Total Votes:':24}{total:10,}{100:11,.2f}%\n"
+    f"{'--':-^46}\n"
+    f"{''.join(lines)}"
+    f"{'--':-^46}\n"
+    f"{'Winner:':10}{winner:>24}{votes[winner]/total * 100:11,.2f}%\n"
+    f"{'--':-^46}\n"
+)
+
+# Assemble the output text file path, starting from the cwd
+output_path = os.path.join(os.path.dirname(__file__), "Analysis", "poll_analysis.csv")
+print(f"\n\n {output_path} \n\n")
+
+# Open the poll_analysis text file and write the report to it
+with open(output_path, "w", encoding="utf-8") as textfile:
+    textfile.write(report)
+
+# Check to see if the report exists and is properly formatted:
+input_path = output_path
+
+# Open the report text file for reading and print it to the terminal
+with open(input_path, "r", encoding="utf-8") as textfile:
+    report = textfile.read()
+
+# And print it out to see if the information, formatting, etc. is correct
+print(f"\n\n{report}\n\n")
